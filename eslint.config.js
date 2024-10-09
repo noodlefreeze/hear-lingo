dd2d42b44fb6e67084c1829c936b2782bfd798ad
noodlefreeze <noodlefreeze@gmail.com>
2024-10-09 13:24:26 +0800

    init

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
+
+export default antfu({
+  ...compat.config({
+    extends: [
+      'plugin:tailwindcss/recommended',
+    ],
+  }),
+})
