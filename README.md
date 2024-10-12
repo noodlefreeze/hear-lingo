9e4380e128aca9053f5cd588a11eed0e692778c2
noodlefreeze <noodlefreeze@gmail.com>
2024-10-12 14:02:08 +0800

    chore: icons

---
+++ README.md
@@ -0,0 +1,1 @@
+TODO: 二分查找 scrollCurrentSubtitleIntoView 函数
---
+++ assets/generate.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="w-[640px] h-[400px] flex justify-center bg-gradient-to-r from-sky-500 to-indigo-500 p-5">
+      <img src={loopControl} alt="" className="rounded-xl" />
+    </div>
+  )
+}
